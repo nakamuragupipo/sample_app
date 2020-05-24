5f933e6a77367f618139749bfb76c2f9d1a53c2d
nakamuragupipo <nakamuragupipo@users.noreply.github.com>
2020-05-24 20:53:24 +0900

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,21 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+   # テストユーザーとしてログインする
+   def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+  end
+end
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
